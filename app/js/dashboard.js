725d7bfe411b277fb5464c8b46f5a0ffdc3b67e4
Chris Saden <csaden@gmail.com>
2015-01-31 08:20:50 -0800

    added data and ctrl and view methods

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -3,61 +3,446 @@ $(function() {
 	var model = {
 
 		init: function() {
+			modelThis = this;
 
-			var modelThis = this;
-			var currentStudent = null;
-			var minutesPerDay,
-				problems,
-				problem_attempts,
-				videos,
-				video_views;
+			//current student data
+			currentStudent = null;
+			modelThis.progressPoints = null;
+			modelThis.attemptedPoints = null;
+			modelThis.progressVideoTime = null;
+			modelThis.attemptedVideoTime = null;
+			modelThis.lecturePointTotals = null;
+			modelThis.lectureVideoTimeTotals = null;
+			modelThis.visits = null;
+			modelThis.avgTimePerDay = null;
+
+			// add this information?
+			modelThis.lastNode = null;
+
+			//csv data
+			modelThis.allProgress = null;
+			modelThis.minutesPerDay = null;
+			modelThis.problems = null;
+			modelThis.problemAttempts = null;
+			modelThis.videos = null;
+			modelThis.videoViews = null;
+
+			//parsed data
+			modelThis.studentIds = [];
+			modelThis.lectures = [];
+			modelThis.details = null;
+			modelThis.courseMap = null;
+
+			//values to compute
+			modelThis.possiblePoints = null;
+			modelThis.possibleVideoTime = null;
+
+			//need to compute this
+			modelThis.medianDailyVisit = null;
 
 			$( document ).ready(function() {
 
-				createDropDown();
+				$(".right.menu.open").on("click", function(e) {
+					e.preventDefault();
+					$(".ui.vertical.menu").toggle();
+				});
+
+				$("#nav a.item").on("click", function() {
+					$(this).addClass("active")
+						.siblings()
+						.removeClass("active");
+				});
 
 				$.when(
 
-					getData("data/minutes_per_day.csv"),
 					getData("data/problems.csv"),
 					getData("data/problem_attempts.csv"),
 					getData("data/videos.csv"),
-					getData("data/video_views.csv")
+					getData("data/video_views.csv"),
+					getData("data/minutes_per_day.csv"),
+					getData("data/all_progress.csv")
 				
-				).done(function(a, b, c, d, e) {
+				).done(function(a, b, c, d, e, f) {
 
-					modelThis.minutesPerDay = processData(a[0]);
-					modelThis.problems = processData(b[0]);
-					modelThis.problem_attempts = processData(c[0]);
-					modelThis.videos = processData(d[0]);
-					modelThis.video_views = processData(e[0]);
+					modelThis.problems = processData(a[0]);
+					modelThis.problemAttempts = processData(b[0]);
+					modelThis.videos = processData(c[0]);
+					modelThis.videoViews = processData(d[0]);
+					modelThis.minutesPerDay = processData(e[0]);
+					modelThis.allProgress = processData(f[0]);
 
+					//parse and format minutesPerDay data to plot later
+					modelThis.minutesPerDay.forEach(function(elem) {
+						modelThis.studentIds.push(+elem.student_id);
+						elem.student_id = +elem.student_id;
+						elem.date = Date.parse(elem.date);
+						elem.minutes_on_site = +elem.minutes_on_site;
+					});
+
+
+					modelThis.studentIds = d3.set(modelThis.studentIds)
+						.values()
+						.map(Number);
+
+					//parse general problems data
+					modelThis.details = d3.nest()
+						.key(function(d) { return d.section; })
+						.key(function(d) { return d.subsection; })
+						.map(modelThis.problems);
+
+					var _allProblems = [];
+
+					for (var weekKey in modelThis.details) {
+						
+						var lectures = modelThis.details[weekKey];
+
+						for (var lectureKey in lectures) {
+
+							newObject = {};
+
+							newObject["week"] = weekKey;
+							newObject["lecture"] = lectureKey;
+							newObject["problems"] = copyArray(lectures[lectureKey]);
+							newObject["videos"] = null;
+							newObject["max_total_time"] = null;
+							newObject["max_total_points"] = d3.sum(lectures[lectureKey], function(d) { return +d.max_points;
+							});
+							_allProblems = Array.prototype.concat(_allProblems, newObject);
+						}
+					}
+
+					modelThis.details = _allProblems;
+
+					//parse general videos data
+					modelThis.videos = d3.nest()
+						.key(function(d) { return d.section; })
+						.key(function(d) { return d.subsection; })
+						.map(modelThis.videos);
+
+					var _allVideos = [];
+
+					for (var vidWeekKey in modelThis.videos) {
+						
+						var vidLectures = modelThis.videos[vidWeekKey];
+
+						for (var vidLectureKey in vidLectures) {
+
+							newObject = {};
+
+							newObject["week"] = vidWeekKey;
+							newObject["lecture"] = vidLectureKey;
+							newObject["videos"] = copyArray(vidLectures[vidLectureKey]);
+							newObject["max_total_time"] = d3.sum(vidLectures[vidLectureKey], function(d) {
+								return +d.duration_seconds;
+							});
+							_allVideos = Array.prototype.concat(_allVideos, newObject);
+						}
+					}
+					modelThis.videos = _allVideos;
+
+					var detailsLength = modelThis.details.length;
+
+					for (var i = 0; i < detailsLength; i++) {
+
+						var thisWeek = modelThis.details[i].week;
+						var thisLecture = modelThis.details[i].lecture;
+
+						if (thisWeek === modelThis.videos[i].week && thisLecture === modelThis.videos[i].lecture) {
+
+							modelThis.details[i]["videos"] = copyArray(modelThis.videos[i].videos);
+							modelThis.details[i]["max_total_time"] = modelThis.videos[i].max_total_time;
+						}
+					}
+
+					modelThis.possiblePoints = d3.sum(modelThis.problems,
+						function(d) {
+							return +d.max_points;
+						});
+
+					modelThis.possibleVideoTime = d3.sum(modelThis.videos, function(d) {
+						return +d.max_total_time;
+					});
+
+					modelThis.videoProgress = d3.nest()
+						.key(function(d) { return d.student_id; })
+						.rollup(function(d) {
+							return {
+								"watched_seconds" : d3.sum(d, function(g) {
+									return +g.watched_seconds;
+								}),
+								"duration_seconds" : d3.sum(d, function(g) {
+									return +g.duration_seconds;
+								})
+							};
+						}).entries(modelThis.videoViews);
+
+					modelThis.courseMap = getCourseMap(modelThis.details);
+
+					modelThis.details.forEach(function(elem) {
+						if (elem.hasOwnProperty("lecture")) {
+							var lectName = elem.lecture.replace(/\s+/g, '-');
+							modelThis.lectures.push(lectName);
+						}
+					});
+					view.init();
 				});
-
-				$(".ui.dropdown").dropdown();
-
 			});
 		}
 	};
 
-	var controller = {
+	var ctrl = {
+		init : function() {
+			model.init();
+		},
+		setCurrentStudent : function(studentId) {
+			model.currentStudent = studentId;
+		},
+		setPointsData : function(studentId) {
+			var problems = modelThis.problemAttempts.filter(function(element) {
+				return element.student_id == studentId;
+			}, studentId);
 
-		getRecordName : function(record) {
-			return record.id;
-		}
+			if (problems.length > 0) {
 
-		updateDashboard : function(record) {
-			$(":input[name='currentStudent']").on("change", function() {
-			//filter data
-			//update the views
-			console.log(this.value);
-});
+				modelThis.progressPoints = d3.sum(problems, function(d) {
+					return +d.score;
+				});
+				modelThis.attemptedPoints = d3.sum(problems, function(d){
+					return +d.max_points;
+				});
+			} else {
+				modelThis.progressPoints = 0;
+				modelThis.attemptedPoints = 0;
+			}
+		},
+		setVideosData : function(studentId) {
+			var videos = modelThis.videoViews.filter(function(element) {
+				return element.student_id == studentId;
+			}, studentId);
+
+			if (videos.length > 0) {
+				modelThis.progressVideoTime = d3.sum(videos, function(d) { return +d.watched_seconds;
+				});
+				modelThis.attemptedVideoTime = d3.sum(videos, function(d) { return +d.duration_seconds;
+				});
+			} else {
+				modelThis.progressVideoTime = 0;
+				modelThis.attemptedVideoTime = 0;
+			}
+		},
+		setVisits : function(studentId) {
+			var visits = modelThis.minutesPerDay.filter(function(element) {
+				return element.student_id === +studentId;
+			}, studentId);
+
+			modelThis.visits = visits;
+		},
+		setAvgTimePerDay : function(studentId) {
+			var avgTimePerDay;
+
+			if (modelThis.visits !== null && modelThis.visits.length > 0) {
+				avgTimePerDay = d3.sum(modelThis.visits, function(d) {
+					return d.minutes_on_site;
+				});
+				avgTimePerDay = Math.round(avgTimePerDay / modelThis.visits.length);
+			} else {
+				avgTimePerDay = 0;
+			}
+			modelThis.avgTimePerDay = avgTimePerDay;
+		},
+		getAvgTimePerDay : function() {
+			return modelThis.avgTimePerDay;
+		},
+		getStudentIds : function() {
+			return modelThis.studentIds;
+		},
+		getCurrentStudent : function() {
+			return model.currentStudent;
+		},
+		getLectures : function() {
+			return modelThis.lectures;
+		},
+		getVisits : function(studentId) {
+			return modelThis.visits;
+		},
+		getAvgTimePerDay : function(studentId) {
+			return modelThis.avgTimePerDay;
+		},
+		getProgressPoints : function() {
+			return modelThis.progressPoints;
+		},
+		getProgressVideoTime : function() {
+			return modelThis.progressVideoTime;
+		},
+		getAttemptedPoints : function() {
+			return modelThis.attemptedPoints;
+		},
+		getAttemptedVideoTime : function () {
+			return modelThis.attemptedVideoTime;
+		},
+		getPossiblePoints: function() {
+			return modelThis.possiblePoints;
+		},
+		getPossibleVideoTime : function() {
+			return modelThis.possibleVideoTime;
+		},
+		getDetails : function() {
+			return modelThis.details;
+		},
+		getCurrentGrade : function() {
+			var grade = Math.round(ctrl.getProgressPoints() / ctrl.getPossiblePoints() * 100);
+			if (55 <= grade && grade <= 64) {
+				grade += "% C";
+			} else if (65 <= grade && grade <= 79) {
+				grade += "% B";
+			} else if (80 <= grade && grade <= 100) {
+				grade += "% A";
+			} else if (grade > 100) {
+				grade += "% A+";
+			} else {
+				grade += "%";
+			}
+			return grade;
+		},
+		updateStudentData : function(studentId) {
+			this.setVideosData(studentId);
+			this.setPointsData(studentId);
+			this.setVisits(studentId);
+			this.setAvgTimePerDay(studentId);
 		}
 	};
 
 	var view = {
+		init : function() {
 
+			this.createDropDown();
+			this.createDetailsTable();
+
+			drawBarContainer("#progress-points-bar", 150, 30);
+			drawBarContainer("#progress-video-time-bar", 150, 30);
+			drawBarContainer("#total-points-bar", 150, 30);
+			drawBarContainer("#total-video-time-bar", 150, 30);
+
+			var lectures = ctrl.getLectures();
+
+			for (var idx in lectures) {
+				var elemId = "#" + lectures[idx];
+				drawBarContainer(elemId, 150, 30);
+				elemId += "-vid";
+				drawBarContainer(elemId, 150, 30);
+			}
+
+			$(":input[name='currentStudent']").on("change", function() {
+				ctrl.setCurrentStudent(this.value);
+				ctrl.updateStudentData(this.value);
+				view.render();
+			});
+		},
+		createDropDown : function() {
+
+			var dropdown = d3.select('#students');
+
+			dropdown.selectAll('item.student')
+				.data(ctrl.getStudentIds())
+				.enter()
+				.append('div')
+				.attr('class', 'item')
+				.attr('data-value', function(d) { return d; })
+				.text(function(d) { return 'Student ' + d; })
+				.html(function(d) {
+					return '<i class="user icon"></i>Student ' + d;
+			});
+
+			$(".ui.dropdown").dropdown();
+		},
+		createDetailsTable : function() {
+
+			var data = ctrl.getDetails();
+
+			var weekRow = d3.select(".progress-details tbody")
+				.selectAll("tr.week-row")
+				.data(data)
+				.enter()
+				.append("tr")
+				.attr("class", "week-row");
+
+			weekRow.append("td")
+				.attr("class", "week")
+				.text(function(d) { return d.week; });
+
+			weekRow.append("td")
+				.attr("class", "lecture")
+				.text(function(d) { return d.lecture; });
+
+			weekRow.append("td")
+				.attr("class", "problem-total")
+				.attr("value", function(d) { return d.max_total_points; });
+
+			weekRow.append("td")
+				.attr("class", "problem-bar")
+				.attr("id", function(d) { 
+					return d.lecture.replace(/\s+/g, '-');
+				});
+
+			weekRow.each(function(d) {
+
+				var current = d3.select(this).append("tr")
+					.attr("class", "problem-set");
+
+				var problemLength = d.problems.length;
+
+				for (var i = 0; i < problemLength; i++) {
+
+					current.append("td")
+						.attr("class", "problem-id")
+						.attr("id", d.problems[i].id)
+						.text("0" + "/" + d.problems[i].max_points);
+				}
+
+			});
+
+			weekRow.append("td")
+				.attr("class", "video-total")
+				.text(function(d) { return d.max_total_time; });
+
+			weekRow.append("td")
+				.attr("class", "video-bar")
+				.attr("id", function(d) {
+					var idName = d.lecture.replace(/\s+/g, '-');
+					idName += "-vid";
+					return idName;
+				});
+		},
+		render : function() {
+			d3.selectAll(".inner-rect").transition()
+				.duration(500)
+				.attr("width", 0)
+				.remove();
+			
+			drawBar("#progress-points-bar", 150, 30, ctrl.getProgressPoints(), ctrl.getAttemptedPoints());
+			
+			drawBar("#progress-video-time-bar", 150, 30, ctrl.getProgressVideoTime(), ctrl.getAttemptedVideoTime());
+			
+			drawBar("#total-points-bar", 150, 30, ctrl.getProgressPoints(), ctrl.getPossiblePoints());
+			
+			drawBar("#total-video-time-bar", 150, 30, ctrl.getProgressVideoTime(), ctrl.getPossibleVideoTime());
+
+			d3.select("#daily-time").text("~" + ctrl.getAvgTimePerDay() + " min");
+			
+			d3.select("#progress-points").text(ctrl.getProgressPoints() + " / " + ctrl.getAttemptedPoints());
+			
+			d3.select("#progress-video-time").text(formatTime(ctrl.getProgressVideoTime()));
+
+			d3.select("#total-attempted-time").text(formatTime(ctrl.getAttemptedVideoTime()));
+			
+			d3.select("#total-points").text(ctrl.getProgressPoints() + " / " + ctrl.getPossiblePoints());
+			
+			d3.select("#total-video-time").text(formatTime(ctrl.getProgressVideoTime()));
+
+			d3.select("#total-possible-time").text(formatTime(ctrl.getPossibleVideoTime()));
+			
+			d3.select("#current-grade").text(ctrl.getCurrentGrade());
+		}
 	};
-
-	controller.init()
+	ctrl.init();
 });
--- app/js/helper.js
+++ app/js/helper.js
@@ -15,28 +15,134 @@ function processData(allText) {
     var lines = [];
 
     for (var i=1; i<allTextLines.length; i++) {
+        
         var data = allTextLines[i].split(',');
         
         if (data.length == headers.length) {
+            
             var tarr = [];
             
             for (var j=0; j<headers.length; j++) {
+            
                 tarr.push(headers[j]+":"+data[j]);
+            
             }
+            
             lines.push(tarr);
         }
     }
 
 	var processedData = [];
+	
 	lines.forEach(function(line) {
+		
 		var newDataObject = {};
+		
 		line.forEach(function(text) {
+			
 			var keyValue = text.split(":");
 			newDataObject[keyValue[0]] = keyValue[1];
 		});
+		
 		processedData.push(newDataObject);
+	
 	});
+	
 	return processedData;
+}
+
+function copyArray(array) {
+
+	newArray = [];
+	arrayLength = array.length;
+
+	for (var idx = 0; idx < arrayLength; idx++) {
+		newArray[idx] = array[idx];
+	}
+	return newArray;
+}
+
+function getCourseMap(detailsArray) {
+
+	var allNodes = [];
+
+	var array = copyArray(detailsArray);
+
+	var arrayLength = array.length;
+
+	for (var i = 0; i < arrayLength; i++) {
+
+		var tempIds = [];
+		var problems = copyArray(array[i].problems);
+		var videos = copyArray(array[i].videos);
+
+		var minIdx = Math.min(problems.length, videos.length);
+
+		for (var j = 0; j < minIdx; j++) {
+
+			var video = videos.shift();
+			tempIds.push(video.id);
+
+			var problem = problems.shift();
+			tempIds.push(problem.id);
+
+		}
+		while (videos.length !== 0) {
+			var vid = videos.shift();
+			tempIds.push(vid.id);
+		}
+		while (problems.length !== 0) {
+			var prob = problems.shift();
+			tempIds.push(prob.id);
+		}
+		allNodes = allNodes.concat(tempIds);
+	}
+	return allNodes;
+}
+
+function drawBarContainer(id, width, height) {
+
+	console.log(id);
+
+	var svg = d3.select(id)
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("rect")
+		.attr("class", "outer-rect")
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("width", width)
+		.attr("height", height)
+		.attr("stroke", "#999")
+		.attr("fill", "white");
+
+}
+
+function drawBar(id, width, height, data, total) {
+
+
+	var length = d3.scale.linear()
+		.domain([0, total])
+		.range([0, width]);
+
+	d3.select(id)
+		.select("svg")
+		.append("rect")
+		.attr("class", "inner-rect")
+		.attr("fill", "#71C3E8")
+		.attr("y", 1)
+		.attr("height", height-2)
+		.attr("width", 0)
+		.transition()
+		.duration(2000)
+		.delay(500)
+		.attr("x", 0)
+		.attr("width", length(data));
+
+		// to round corners of rectangles
+		// .attr("rx", radius)
+		// .attr("ry", radius)
 }
 
 function getMaxObjectValue(this_array, element) {
@@ -63,35 +169,18 @@ function getMinObjectValue(this_array, element) {
     return values[0];
 }
 
-function createBar(idToAppendTo, width, height) {
+function formatTime(num) {
 
-	var elem = d3.select(idToAppendTo);
-	elem.append("svg")
-		.width(width)
-		.height(height)
-		.append("rect")
-		.attr("class", "outer-rect")
-		.attr("x", 0)
-		.attr("y", height/2)
-		.width(width)
-		.height(height)
-		.append("x", 0)
-		.append("y", height/2)
-		.width(width)
-		.height(height)
-		.fill("gray");
-}
+    var sec_num = parseInt(+num, 10);
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-Array.prototype.unique = function() {
-	var n = {}, r = [];
-	
-	for(var i = 0; i < this.length; i++) {
-		
-		if (!n[this[i]]) {
-			n[this[i]] = true;
-			r.push(this[i]);
-		}
-	}
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
 
-	return r;
+    var time    = hours+':'+minutes+':'+seconds;
+
+    return time;
 };
